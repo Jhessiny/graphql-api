97dc93f73dd520eaf2d746bd0e9a533223e725d0
Jhessiny <jhessinymattos@gmail.com>
2022-03-18 15:09:42 -0300

    chore: add cors

--- src/server.js
+++ src/server.js
@@ -3,8 +3,13 @@ const app = express();
 const PORT = 6969;
 const { graphqlHTTP } = require("express-graphql");
 const graphqlSchema = require("./graphql");
+const cors = require("cors");
 
-app.use("/graphql", graphqlHTTP({ schema: graphqlSchema, graphiql: true }));
+app.use(
+  "/graphql",
+  cors(),
+  graphqlHTTP({ schema: graphqlSchema, graphiql: true })
+);
 
 app.listen(PORT, () => {
   console.log("Server running");
